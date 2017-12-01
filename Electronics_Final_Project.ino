5a9a431cf3a2c5f81ca3d7e4ba3b3bc1c09b5222
“Logan <logans@goshen.edu>
2017-11-30 21:55:09 -0500

    Added the 'rainbowCycle' function.

--- Electronics_Final_Project.ino
+++ Electronics_Final_Project.ino
@@ -383,6 +383,22 @@ void rainbow(uint8_t wait) {
   }
 }
 
+void rainbowCycle(uint8_t wait) {
+  int i, j;
+  
+  for (j=0; j < 256 * 5; j++) {     // 5 cycles of all 25 colors in the wheel
+    for (i=0; i < strip.numPixels(); i++) {
+      // tricky math! we use each pixel as a fraction of the full 96-color wheel
+      // (thats the i / strip.numPixels() part)
+      // Then add in j which makes the colors go around per pixel
+      // the % 96 is to make the wheel cycle around
+      strip.setPixelColor(i, Wheel( ((i * 256 / strip.numPixels()) + j) % 256) );
+    }  
+    strip.show();   // write all the pixels out
+    delay(wait);
+  }
+}
+
 uint32_t Wheel(byte WheelPos)
 {
   if (WheelPos < 85) {
