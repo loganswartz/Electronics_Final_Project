5ede990a9f8638d2557c154539c861c64e9fdd29
“Logan <logans@goshen.edu>
2017-12-12 00:38:22 -0500

    added in a ton of comments and included the rest of the needed libraries for better portability.

--- Electronics_Final_Project.ino
+++ Electronics_Final_Project.ino
@@ -4,6 +4,9 @@
  *  
  *  This project is hosted as a GitHub repo at https://github.com/loganswartz/Electronics_Final_Project, including
  *  all the necessary libraries.
+ *  
+ *  This project was completed as the final project of our Fall 2017 Electronics class. RIP our first light strand, you
+ *  will not be forgotten. Wiring schematic can be found here: http://loganswartz.com/resources/20171211_161803.jpg
  */
 
 #include <Adafruit_WS2801.h>              // Included in GitHub repo or find it at https://learn.adafruit.com/12mm-led-pixels/code
